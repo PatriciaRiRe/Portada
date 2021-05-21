c7c13ab90f6e4c708f364cfa1c1f0afa9f0d4445
paty6 <paty6@LAPTOP-PQNMHTVG>
2021-05-20 19:55:31 -0500

    Portada

---
+++ src/main/java/com/patricia/Controllers/DemoController.java
@@ -0,0 +1,16 @@
+package com.patricia.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	
+
+	@GetMapping("/")
+	public String Portada() {
+		return "Portada";
+	}
+	
+}
